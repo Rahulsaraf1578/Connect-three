a67a81ec9d10293768e13448b52855d13c325f28
Rahulsaraf1578 <rahulsaraf1578@gmail.com>
2025-01-06 16:27:07 +0530

    data binding

--- src/app/level2/level2.component.ts
+++ src/app/level2/level2.component.ts
@@ -8,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './level2.component.css'
 })
 export class Level2Component {
-  public testField="X";
+  private currentPlayerIndex:number;
+  private currentWinnerIndex:number;
+  public boardContent:number[][];
+  private playerNames:string[];
+
+  constructor(){
+    this.playerNames = ['','X','O'];
+
+    this.boardContent = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+
+    this.currentPlayerIndex=1;
+    this.currentWinnerIndex=0;
+  }
+
+  public getPlayerName(col:number, row:number):string{
+    return this.playerNames[this.boardContent[row][col]];
+  }
+
+  public getStyle(col:number, row:number):string{
+    if(this.boardContent[row][col]!==0){
+      return `occupied-${this.getPlayerName(row,col)}`
+    }
+    return '';
+  }
+
+  public set(row:number, col:number):void{
+    if(this.currentWinnerIndex === 0 && this.boardContent[row][col] === 0){
+      this.boardContent[row][col] = this.currentPlayerIndex;
+      this.currentPlayerIndex = this.currentPlayerIndex === 1?2:1;
+    }
+  }
 }
